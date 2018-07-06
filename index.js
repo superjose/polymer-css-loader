80022ce8970041b18c35df8d96cbbd8da7decc52
superjose <superjose_49@hotmail.com>
2018-07-06 00:29:27 -0400

    Eliminated the console.log() s

--- index.js
+++ index.js
@@ -10,20 +10,15 @@ module.exports = function (source) {
   // Get the options from webpack.config.js
   const options = getOptions(this) || {};
 
-  console.log('These are the options', options);
-
   // File-Loader does this.
   const context = this.rootContext || (this.options & this.options.context);
-  console.log('here');
   // The name of the element that we're going to register.
   // this.resourceQuery in the form of: mystyle.css?name=my-custom-name
   const registrationName = nameParser(this.resourceQuery);
-  console.log('here 2');
   const name = interpolateName(this, registrationName, {
     context,
     content: source,
   });
-  console.log('here 3');
   const temp = template(name, source, options);
   
   // Generates the JavaScript required for Web Components
